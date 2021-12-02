477e79baf08436cf7df4723fbb3f15fe2171984d
CTRLFE4R <94206555+CTRLFE4R@users.noreply.github.com>
2021-12-01 18:53:06 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var jakeImg, pathImg, path, jake, left_boundary, right_boundary, subway, subwayImg, coin, coinImg, coinscore,song,slider;
+function preload(){
+ 
+  jakeImg=loadAnimation("jake1.png", "jake2.png", "jake3.png", "jake4.png", "jake5.png");
+  pathImg=loadImage("path.png");
+  
+}
+
+function setup(){
+  createCanvas(400,400);
+  path=createSprite(200,200);
+  path.addImage(pathImg);
+  path.velocityY=5;
+  path.y=path.height/30;
+  jake=createSprite(180,340,50,170);
+  jake.addAnimation("running", jakeImg);
+  left_boundary=createSprite(0,300,100,600);
+  right_boundary=createSprite(390,300,80,600);
+ left_boundary.visible=false;
+right_boundary.visible=false;
+}
+
+function draw() {
+  background(0);
+  textSize(10);
+  path.velocityY=5;
+  jake.x=World.mouseX;
+  if(path.y>400) {
+  path.y=height/2 ;
+  if(jake.isTouching(coin)) {
+  coinscore = coinscore+1;
+ }
+  
+}
+  jake.collide(left_boundary);
+  jake.collide(right_boundary);
+  edges=createEdgeSprites();
+  jake.collide(edges[3]);
+drawSprites();
+}
+
+
